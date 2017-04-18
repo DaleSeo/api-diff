b76b99cbdb5e151ceba0e464056b55cab2e11a22
Dale Seo <dale.seo@gmail.com>
2017-04-18 22:36:40 +0900

    import EcData

--- src/data/importEcData.js
+++ src/data/importEcData.js
@@ -3,15 +3,26 @@ import db from '../services/database'
 
 let specRef = db.ref('apis/-KhQ6MTyGDA-W1CaALev/specs')
 
-json.apis.forEach(api => {
-  db.ref('apis/-KhQ6MTyGDA-W1CaALev/specs')
-    .push({
-      method: api.method,
-      url: api.path,
-      body: api.body || {},
-      skip: api.skip,
-      text: JSON.stringify(api.body, null, 2) || '',
-      title: api.description || '',
-      exclusion: api.ignoreKeys || []
+specRef.remove()
+  .then(_ => Promise.all(
+    json.apis.map(api => {
+      return specRef.push({
+        method: api.method,
+        url: api.path,
+        body: api.body || {},
+        skip: api.skip,
+        text: JSON.stringify(api.body, null, 2) || '',
+        title: api.description || '',
+        exclusion: api.ignoreKeys || []
+      })
     })
-})
+  ))
+  .then(vals => vals.map(val => val.key))
+  .then(keys => {
+    console.log(keys)
+    db.goOffline()
+  })
+  .catch(err => {
+    console.error(err)
+    db.goOffline()
+  })
