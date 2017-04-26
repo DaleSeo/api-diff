0f3644d8d18ddd51265a36cc1f8e1f0e8459ee81
Dale Seo <dale.seo@gmail.com>
2017-04-26 11:14:09 +0900

    Headers&Body Exclusios

--- src/components/settings/ExclusionsHeaders.vue
+++ src/components/settings/ExclusionsHeaders.vue
@@ -1,63 +1,40 @@
 <template>
-  <div class="panel panel-primary">
-    <div class="panel-heading">
-      <strong>헤더 전역 예외 항목</strong> <small v-if="exclusions">({{exclusions.length}})</small>
-    </div>
-    <ul class="list-group">
-      <li class="list-group-item" v-for="exclusion in exclusions">
-        <span>{{exclusion}}</span>
-        <button type="button" class="close" @click="del(exclusion)">&times;</button>
-      </li>
-    </ul>
-    <div class="panel-footer">
-      <div class="input-group">
-        <input type="text" class="form-control input-sm" placeholder="추가할 예외 필드를 입력하세요." v-model="newExclusion" @keyup.enter="add"/>
-        <span class="input-group-btn">
-          <button class="btn btn-primary btn-sm" type="button" :disabled="disabled" @click="add">
-            <i class="fa fa-pencil-square-o"/> 추가
-          </button>
-        </span>
-      </div>
-    </div>
-  </div>
+  <ListForm
+    variant="primary"
+    title="헤더 전역 예외"
+    :items="exclusions"
+    @add="add"
+    @del="del"
+  />
 </template>
 
 <script>
+import ListForm from '../common/ListForm.vue'
+
 import db from '../../services/database'
 
 export default {
+  components: {ListForm},
   computed: {
-    disabled () {
-      return this.newExclusion.trim().length == 0
-    },
     exclusions () {
       return this.exclustionsObj['.value'] || []
-    }
-  },
-  data () {
-    return {
-      newExclusion: ''
     }
   },
   firebase () {
     return {
       exclustionsObj: {
-        source: db.ref('settings/exclusions'),
+        source: db.ref('settings/headersExclusions'),
         asObject: true
       }
     }
   },
   methods: {
-    add () {
-      if (this.disabled) return
-      this.exclusions.push(this.newExclusion)
+    add (exclusion) {
+      this.exclusions.push(exclusion)
       this.$firebaseRefs.exclustionsObj.set(this.exclusions)
-      this.newExclusion = ''
     },
     del (exclusion) {
-      console.log(exclusion)
       let exclusions = this.exclusions.filter(e => e !== exclusion)
-      console.log(exclusions)
       this.$firebaseRefs.exclustionsObj.set(exclusions)
     }
   }
