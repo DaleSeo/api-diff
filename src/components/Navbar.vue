ca78fd65673b1ecc92d9119433ad99800e9184c3
Dale Seo <dale.seo@gmail.com>
2017-05-02 15:25:39 +0900

    retitle

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/"><b>API Diff</b>&nbsp;&nbsp;&nbsp;<small>API 비교 도구</small></a>
+        <a class="navbar-brand" href="/"><b>One Tests</b></a>
       </div>
       <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav navbar-right">
